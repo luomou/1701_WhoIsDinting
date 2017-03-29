7ac2bd90aae5a38eaf23b1a6815c48fa488b706e
xiangtanluochao <xiangtanluochao@126.com>
2017-03-29 21:41:06 +0800

    测试一下

--- src/com/thread/SSWDFrameThread.java
+++ src/com/thread/SSWDFrameThread.java
@@ -2,13 +2,12 @@ package com.thread;
 
 import java.net.Socket;
 
-import com.hx.socket.ServerThread;
-
 public class SSWDFrameThread extends ServerThread {
 
 	public SSWDFrameThread(Socket socket, Socket socket1) {
 		super(socket, socket1);
 		// TODO Auto-generated constructor stub
+		// aslkasjfkjaskfkasdf
 	}
 
 }
